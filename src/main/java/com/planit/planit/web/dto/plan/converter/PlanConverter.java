346cae440b3d578e500b79cda3894c10692b74c2
dvlp-sy <sy8389b@gmail.com>
2025-05-26 15:09:10 +0900

    [PL-60] Add: "Plan 요청 응답 DTO 생성"

---
+++ src/main/java/com/planit/planit/web/dto/plan/converter/PlanConverter.java
@@ -0,0 +1,67 @@
+package com.planit.planit.web.dto.plan.converter;
+
+import com.planit.planit.plan.Plan;
+import com.planit.planit.web.dto.plan.PlanRequestDTO;
+import com.planit.planit.web.dto.plan.PlanResponseDTO;
+import com.planit.planit.web.dto.task.converter.TaskConverter;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class PlanConverter {
+
+    public static PlanRequestDTO.PlanDTO toPlanDTO(Plan plan) {
+        return PlanRequestDTO.PlanDTO.builder()
+                .title(plan.getTitle())
+                .motivation(plan.getMotivation())
+                .icon(plan.getIcon())
+                .planStatus(plan.getPlanStatus())
+                .startedAt(plan.getStartedAt())
+                .finishedAt(plan.getFinishedAt())
+                .build();
+    }
+
+    public static PlanResponseDTO.PlanPreviewDTO toPlanPreviewDTO(Plan plan) {
+        String dDay = formatDDay(LocalDateTime.now(), plan.getFinishedAt());
+        return PlanResponseDTO.PlanPreviewDTO.builder()
+                .planId(plan.getId())
+                .title(plan.getTitle())
+                .icon(plan.getIcon())
+                .totalTasks(plan.countTasks())
+                .dDay(dDay)
+                .build();
+    }
+
+    public static PlanResponseDTO.PlanContentDTO toPlanDetailDTO(Plan plan) {
+        return PlanResponseDTO.PlanContentDTO.builder()
+                .planId(plan.getId())
+                .title(plan.getTitle())
+                .icon(plan.getIcon())
+                .motivation(plan.getMotivation())
+                .tasks(plan.getTasks().stream()
+                        .map(TaskConverter::toTaskPreviewDTO)
+                        .toList())
+                .build();
+    }
+
+    public static PlanResponseDTO.PlanMetaDTO toPlanMetaDTO(Plan plan) {
+        return PlanResponseDTO.PlanMetaDTO.builder()
+                .planId(plan.getId())
+                .title(plan.getTitle())
+                .icon(plan.getIcon())
+                .motivation(plan.getMotivation())
+                .planStatus(plan.getPlanStatus())
+                .startedAt(plan.getStartedAt())
+                .finishedAt(plan.getFinishedAt())
+                .build();
+    }
+
+    private static String formatDDay(LocalDateTime startedAt, LocalDateTime finishedAt) {
+        Long day = ChronoUnit.DAYS.between(startedAt, finishedAt);
+        if (startedAt.isAfter(finishedAt)) {
+            return String.format("D+%d", -day);
+        } else {
+            return String.format("D-%d", day);
+        }
+    }
+}
