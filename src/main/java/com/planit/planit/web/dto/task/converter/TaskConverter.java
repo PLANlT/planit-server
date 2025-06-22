a4861755e1d46ca001ce1579efc1498c5b755145
dvlp-sy <sy8389b@gmail.com>
2025-06-22 14:04:48 +0900

    [PL-104] Task: "TaskDTO 생성"

--- src/main/java/com/planit/planit/web/dto/task/converter/TaskConverter.java
+++ src/main/java/com/planit/planit/web/dto/task/converter/TaskConverter.java
@@ -1,7 +1,12 @@
 package com.planit.planit.web.dto.task.converter;
 
+import com.planit.planit.plan.Plan;
 import com.planit.planit.task.Task;
+import com.planit.planit.task.association.CompletedTask;
 import com.planit.planit.web.dto.task.TaskResponseDTO;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class TaskConverter {
 
@@ -10,6 +15,39 @@ public class TaskConverter {
                 .taskId(task.getId())
                 .taskType(task.getTaskType())
                 .title(task.getTitle())
+                .build();
+    }
+
+    public static TaskResponseDTO.TaskRoutineDTO toTaskRoutineDTO(Task task) {
+        return TaskResponseDTO.TaskRoutineDTO.builder()
+                .taskId(task.getId())
+                .taskType(task.getTaskType())
+                .routineDay(task.getRoutineDay())
+                .routineTime(task.getRoutineTime())
+                .build();
+    }
+
+    public static List<TaskResponseDTO.TaskStatusDTO> toTodayTaskListDTO(Plan plan, LocalDate today) {
+        return plan.getTasks().stream()
+                // 플랜에서 오늘 루틴에 해당하는 작업만 필터링
+                .filter(task -> task.getRoutineDay().equals(today.getDayOfWeek()))
+                        // 오늘 완료된 작업이 있으면 true로 표시
+                        .map(task -> {
+                            List<Task> tasks = task.getCompletedTasks().stream()
+                                    .filter(completedTask -> completedTask.getCompletedAt().equals(today))
+                                    .map(CompletedTask::getTask)
+                                    .toList();
+                            return toTaskStatusDTO(task, !tasks.isEmpty());
+                        })
+                        .toList();
+    }
+
+    public static TaskResponseDTO.TaskStatusDTO toTaskStatusDTO(Task task, Boolean isCompleted) {
+        return TaskResponseDTO.TaskStatusDTO.builder()
+                .taskId(task.getId())
+                .title(task.getTitle())
+                .routineTime(task.getRoutineTime())
+                .isCompleted(isCompleted)
                 .build();
     }
 }
