e655bd199626a7c23fcbc6727d75e0821d52e4f8
Yoonhojoon <dbsghwns1209@gmail.com>
2025-06-26 01:05:23 +0900

    [PL-116] Test: "MemberServiceImpl 테스트 코드 작성 (실패)"

---
+++ src/main/java/com/planit/planit/member/service/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package com.planit.planit.member.service;
+
+
+import com.planit.planit.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+}
---
+++ src/test/java/com/planit/planit/auth/FakeOAuth2User.java
@@ -0,0 +1,31 @@
+package com.planit.planit.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class FakeOAuth2User implements OAuth2User {
+    private final Map<String, Object> attributes;
+
+    public FakeOAuth2User(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.getOrDefault("name", "unknown");
+    }
+}
