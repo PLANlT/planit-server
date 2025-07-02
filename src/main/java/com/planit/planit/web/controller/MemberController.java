8e8aed08b14f65dd8dc2e9802ef40a26942625db
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-02 23:51:09 +0900

    [PL-132] test: MemberController 테스트 코드 작성

---
+++ src/main/java/com/planit/planit/web/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.planit.planit.web.controller;
+
+
+import com.planit.planit.member.service.MemberService;
+import com.planit.planit.web.dto.auth.login.converter.OAuthLoginDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+
+    public ResponseEntity<OAuthLoginDTO.Response> signIn(OAuthLoginDTO.Request request) {
+
+        return null;
+    }
+}
