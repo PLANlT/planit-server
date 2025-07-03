f1225ac4a516d834bb313f2a2697062e42a58317
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-04 02:34:11 +0900

    [PL-143] chore: "의미없는 RefreshTokenRedisEntity.java 삭제"

--- src/main/java/com/planit/planit/redis/repository/RefreshTokenRedisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.planit.planit.redis.repository;
-
-import com.planit.planit.redis.entity.RefreshTokenRedisEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshTokenRedisEntity, String> {
-} 
