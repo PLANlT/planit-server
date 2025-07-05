d41bd8f05055e620d44549324164d05e59bd32d4
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-06 01:26:51 +0900

    [PL-149] chore: "FakeCustomOAuth2User 추가"

--- src/test/java/com/planit/planit/auth/FakeCustomOAuth2User.java
+++ src/test/java/com/planit/planit/auth/FakeCustomOAuth2User.java
@@ -1,13 +1,13 @@
-package com.planit.planit.auth;
+    package com.planit.planit.auth;
 
-import com.planit.planit.config.oauth.CustomOAuth2User;
-import com.planit.planit.member.enums.SignType;
+    import com.planit.planit.config.oauth.CustomOAuth2User;
+    import com.planit.planit.member.enums.SignType;
 
-import java.util.Map;
+    import java.util.Map;
 
-public class FakeCustomOAuth2User extends CustomOAuth2User {
+    public class FakeCustomOAuth2User extends CustomOAuth2User {
 
-    public FakeCustomOAuth2User(Map<String, Object> attributes, SignType signType) {
-        super(new FakeOAuth2User(attributes), signType);
+        public FakeCustomOAuth2User(Map<String, Object> attributes, SignType signType) {
+            super(new FakeOAuth2User(attributes), signType);
+        }
     }
-}
