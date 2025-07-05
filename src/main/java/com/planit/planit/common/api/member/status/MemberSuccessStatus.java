8dceecd06f8c4766f115247229017f72e7f3fa7f
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-06 03:25:56 +0900

    [PL-149] chore: "MemberSuccessStatus 상태코드 추가 및 변경"

--- src/main/java/com/planit/planit/common/api/member/status/MemberSuccessStatus.java
+++ src/main/java/com/planit/planit/common/api/member/status/MemberSuccessStatus.java
@@ -5,10 +5,15 @@ import org.springframework.http.HttpStatus;
 
 public enum MemberSuccessStatus implements SuccessResponse {
 
+    // 로그인/회원가입
+    SIGN_IN_SUCCESS(HttpStatus.OK, "MEMBER2000", "로그인이 완료되었습니다."),
+    SIGN_UP_SUCCESS(HttpStatus.CREATED, "MEMBER2001", "회원가입이 완료되었습니다."),
+    SIGN_OUT_SUCCESS(HttpStatus.OK, "MEMBER2002", "로그아웃이 완료되었습니다."),
+
     // 길티프리
-    GUILTY_FREE_SET(HttpStatus.OK, "MEMBER2001", "길티프리를 활성화하였습니다."),
-    GUILTY_FREE_FOUND(HttpStatus.OK, "MEMBER2002", "길티프리 활성일을 조회하였습니다."),
-    GUILTY_FREE_REASON_LIST_FOUND(HttpStatus.OK, "MEMBER2003", "길티프리 사유 목록을 조회하였습니다."),
+    GUILTY_FREE_SET(HttpStatus.OK, "MEMBER2010", "길티프리를 활성화하였습니다."),
+    GUILTY_FREE_FOUND(HttpStatus.OK, "MEMBER2011", "길티프리 활성일을 조회하였습니다."),
+    GUILTY_FREE_REASON_LIST_FOUND(HttpStatus.OK, "MEMBER2012", "길티프리 사유 목록을 조회하였습니다."),
 
     ;
 
