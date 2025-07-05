a12a6be44779b80ff4f266dfd220df575cbcf5b3
dvlp-sy <sy8389b@gmail.com>
2025-07-05 15:09:34 +0900

    [PL-153] Feat: "길티프리 관련 API 구현"

---
+++ src/main/java/com/planit/planit/member/service/GuiltyFreeService.java
@@ -0,0 +1,11 @@
+package com.planit.planit.member.service;
+
+import com.planit.planit.member.enums.GuiltyFreeReason;
+import com.planit.planit.web.dto.member.guiltyfree.GuiltyFreeResponseDTO;
+
+public interface GuiltyFreeService {
+
+    GuiltyFreeResponseDTO.GuiltyFreeActivationDTO activateGuiltyFree(Long memberId, GuiltyFreeReason reason);
+
+    GuiltyFreeResponseDTO.GuiltyFreeActivationDTO getGuiltyFreeStatus(Long memberId);
+}
