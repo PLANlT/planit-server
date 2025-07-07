e6ccd52470e1e27d29fe8d2488785800fba4a57e
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-08 01:42:18 +0900

    [PL-171] chore: "TermAgreementDTO schema 변경"

--- src/main/java/com/planit/planit/web/dto/member/term/TermAgreementDTO.java
+++ src/main/java/com/planit/planit/web/dto/member/term/TermAgreementDTO.java
@@ -1,6 +1,7 @@
 package com.planit.planit.web.dto.member.term;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,17 +14,22 @@ public class TermAgreementDTO {
     @AllArgsConstructor
     @Builder
     public static class Request {
+        // 만약 이 필드들이 LocalDateTime 타입이라면 아래처럼 설정
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @Schema(type = "string", example = "2025-07-08T01:17:17") // 원하는 형식의 예시 값
         private LocalDateTime termOfUse;
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @Schema(type = "string", example = "2025-07-08T01:17:17")
         private LocalDateTime termOfPrivacy;
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @Schema(type = "string", example = "2025-07-08T01:17:17")
         private LocalDateTime termOfInfo;
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        private LocalDateTime overFourteen;
+        @Schema(type = "string", example = "2025-07-08T01:17:17")
+        private LocalDateTime overFourteen; // 이 필드도 LocalDateTime이라면
     }
 
     @Getter
