e95c1c8b7d29e44a3e8e52c8d20a32463e994f31
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-12 04:36:35 +0900

    [PL-174] chore: "MemberInfoResponseDTO로 이름 변경"

--- src/main/java/com/planit/planit/web/dto/member/MemberInfoResponseDTO.java
+++ src/main/java/com/planit/planit/web/dto/member/MemberInfoResponseDTO.java
@@ -6,28 +6,38 @@ import com.planit.planit.member.enums.SignType;
 import lombok.Builder;
 import lombok.Getter;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
 @Getter
-public class MemberInfoDTO {
+public class MemberInfoResponseDTO {
     private Long id;
     private String email;
     private String name;
     private String signType;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
+
     @Builder
-    public MemberInfoDTO(Long id, String email, String name, SignType signType) {
+    public MemberInfoResponseDTO(Long id, String email, String name, SignType signType, LocalDate createdAt) {
         this.id = id;
         this.email = email;
         this.name = name;
         this.signType = String.valueOf(signType);
+        this.createdAt = createdAt;
     }
 
-    public static MemberInfoDTO of(Member member) {
-        return MemberInfoDTO.builder()
+    public static MemberInfoResponseDTO of(Member member) {
+        return MemberInfoResponseDTO.builder()
                 .id(member.getId())
                 .email(member.getEmail())
                 .name(member.getMemberName())
                 .signType(member.getSignType())
+                .createdAt(member.getCreatedAt().toLocalDate()) // 여기 중요!
                 .build();
     }
-
 }
