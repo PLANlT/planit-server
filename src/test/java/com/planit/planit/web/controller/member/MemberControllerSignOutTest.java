995ad2485fa675713c05f71ee2b51b772916c540
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-12 21:02:06 +0900

    [PL-176] test: "./gradlew test 안되는 오류 수정"

--- src/test/java/com/planit/planit/web/controller/member/MemberControllerSignOutTest.java
+++ src/test/java/com/planit/planit/web/controller/member/MemberControllerSignOutTest.java
@@ -1,5 +1,6 @@
 package com.planit.planit.web.controller.member;
 
+import com.planit.planit.agreement.service.AgreementService;
 import com.planit.planit.common.api.general.GeneralException;
 import com.planit.planit.common.api.general.status.ErrorStatus;
 import com.planit.planit.config.jwt.JwtProvider;
@@ -7,7 +8,9 @@ import com.planit.planit.config.jwt.UserPrincipal;
 import com.planit.planit.config.oauth.CustomOAuth2UserService;
 import com.planit.planit.member.enums.Role;
 import com.planit.planit.member.repository.MemberRepository;
+import com.planit.planit.member.service.FcmTokenService;
 import com.planit.planit.member.service.MemberService;
+import com.planit.planit.member.service.NotificationService;
 import com.planit.planit.web.controller.MemberController;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,14 +38,12 @@ class MemberControllerSignOutTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private MemberService memberService;
-    @MockBean
-    private JwtProvider jwtProvider;
-    @MockBean
-    private MemberRepository memberRepository;
-    @MockBean
-    private CustomOAuth2UserService customOAuth2UserService;
+    @MockBean private MemberService memberService;
+    @MockBean private JwtProvider jwtProvider;
+    @MockBean private MemberRepository memberRepository;
+    @MockBean private FcmTokenService fcmTokenService;
+    @MockBean private NotificationService notificationService;
+    @MockBean private AgreementService agreementService;
 
     @AfterEach
     void clearSecurityContext() {
--- src/test/java/com/planit/planit/web/controller/member/security/MemberControllerSignOutSecurityTest.java
+++ src/test/java/com/planit/planit/web/controller/member/security/MemberControllerSignOutSecurityTest.java
@@ -1,11 +1,14 @@
 package com.planit.planit.web.controller.member.security;
 
+import com.planit.planit.agreement.service.AgreementService;
 import com.planit.planit.config.SecurityConfig;
 import com.planit.planit.member.repository.MemberRepository;
+import com.planit.planit.member.service.FcmTokenService;
 import com.planit.planit.member.service.MemberService;
 import com.planit.planit.config.jwt.JwtProvider;
 import com.planit.planit.config.oauth.CustomOAuth2UserService;
 
+import com.planit.planit.member.service.NotificationService;
 import com.planit.planit.web.controller.MemberController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,28 +22,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MemberController.class)
-@Import(SecurityConfig.class) // ✅ 필터 포함
-@DisplayName("MemberControllerSecurityTest - Spring Security 예외 테스트")
-class MemberControllerSecurityTest {
+@Import(SecurityConfig.class)
+class MemberControllerSignOutSecurityTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    // 🔧 SecurityConfig 내 필요한 빈들 Mocking
-    @MockBean
-    private MemberService memberService;
-    @MockBean
-    private JwtProvider jwtProvider;
-    @MockBean
-    private MemberRepository memberRepository;
-    @MockBean
-    private CustomOAuth2UserService customOAuth2UserService;
+    @MockBean private MemberService memberService;
+    @MockBean private JwtProvider jwtProvider;
+    @MockBean private MemberRepository memberRepository;
+    @MockBean private FcmTokenService fcmTokenService;
+    @MockBean private NotificationService notificationService;
+    @MockBean private AgreementService agreementService;
 
     @Test
     @DisplayName("인증되지 않은 사용자가 로그아웃 요청 시 401 Unauthorized를 반환한다")
     void signOut_unauthenticatedUser_returnsUnauthorized() throws Exception {
         mockMvc.perform(post("/members/sign-out"))
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().is(403));
     }
-
 }
