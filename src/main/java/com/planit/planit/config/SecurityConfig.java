57bcdb14fe162c13b401f3200ee655663f5fa059
Yoonhojoon <dbsghwns1209@gmail.com>
2025-07-13 23:34:48 +0900

    Hotfix: "약관 이름 통일 및 접속 안되는 문제 해결"

--- src/main/java/com/planit/planit/config/SecurityConfig.java
+++ src/main/java/com/planit/planit/config/SecurityConfig.java
@@ -29,6 +29,8 @@ public class SecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/planit/auth/sign-in", "POST")).permitAll()     // 로그인 API
                 .requestMatchers(new AntPathRequestMatcher("/planit/auth/refresh", "POST")).permitAll()     // 토큰 재발급 API
                 .requestMatchers(new AntPathRequestMatcher("/planit/members/terms", "GET")).permitAll()    // 약관 URL 조회 API (인증 없이 허용)
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+
                 .anyRequest().authenticated()
         );
 
--- src/test/java/com/planit/planit/member/service/MemberServiceImplTest.java
+++ src/test/java/com/planit/planit/member/service/MemberServiceImplTest.java
@@ -1,15 +1,19 @@
 package com.planit.planit.member.service;
 
+import com.planit.planit.auth.oauth.SocialTokenVerifier;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Transactional
 class MemberServiceImplTest {
+    @MockBean
+    private SocialTokenVerifier socialTokenVerifier;
 
     @InjectMocks
     private MemberServiceImpl memberServiceImpl;
